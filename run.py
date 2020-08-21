6b24a97d240af13e80f85113eb264f265b7be556
jongjetw <jongjet.w@gmail.com>
2020-08-22 00:43:12 +1000

    fix comment

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import os
 
 #scan footage file
-#list_files = os.listdir('Footages/')
+list_files = os.listdir('Footages/')
 
 #run openCV for each file
-#for file in list_files:
-#    os.system('time python3 crowd_counting.py --input Footages/'+ file +' --display 1')
+for file in list_files:
+    os.system('time python3 crowd_counting.py --input Footages/'+ file +' --display 1')
 
 #read and create data frame
 os.system('python3 readTxt.py')
